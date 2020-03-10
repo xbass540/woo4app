37b9682e3c384a87111dfaa017e7778cc9eb08e3
xbass540 <info@solomonsoft.gr>
2020-03-10 17:09:25 +0200

    lec28 working

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ModalController } from 'ionic-angular';
-
+import { NavController, NavParams, ModalController, ViewController } from 'ionic-angular';
+import {Storage} from '@ionic/storage';
 
 
 @Component({
@@ -9,11 +9,66 @@ import { NavController, NavParams, ModalController } from 'ionic-angular';
 })
 export class CartPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cartItems:any=[];
+  total:any;
+  showEmptyCartMessage: boolean = false;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage:Storage, public viewCtrl: ViewController) {
+
+
+    this.total = 0.0;
+    
+    this.storage.ready().then(()=>{
+
+      this.storage.get("cart").then( (data)=>{
+        this.cartItems = data;
+        console.log(this.cartItems);
+
+        if(this.cartItems.length > 0){
+
+          this.cartItems.forEach( (item, index)=> {
+            this.total = this.total + (item.product.price * item.qty)
+          })
+
+        } else {
+
+          this.showEmptyCartMessage = true;
+
+        }
+
+
+      })
+
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CartPage');
+  }
+
+  removeFromCart(item, i){
+
+    let price = item.product.price;
+    let qty = item.qty;
+
+    this.cartItems.splice(i, 1);
+
+    this.storage.set("cart", this.cartItems).then( ()=> {
+
+      this.total = this.total - (price * qty);
+
+    });
+
+    if(this.cartItems.length == 0){
+      this.showEmptyCartMessage = true;
+    }
+
+
+  }
+
+  closeModal(){
+    this.viewCtrl.dismiss();
   }
 
 }
